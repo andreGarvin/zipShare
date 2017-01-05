8c5ea1425db715b8b56a02ea77e7f3592b3d71f2
Dre_ <andre.garvin@envirostudies.org>
2017-01-05 21:43:53 +0000

    upadte 2.0

--- webserver.py
+++ webserver.py
@@ -1,15 +1,25 @@
+# server
 from flask import Flask, request, render_template, jsonify, send_file
+
+# time stamp
 from time import gmtime, strftime
 from random import randint
+
+# upload and download
 from io import BytesIO
+
+# josn
 import json
+
+# operating system
 import os
 
-
+# start app
 app = Flask(__name__)
 
 
 db = {
+    # active kyes being used
   'activeKeys': {
       'count': 0,
       'keys': []
@@ -53,7 +63,8 @@ def home():
             'content': content,
             'file_size': len( content ),
             'file_key': db['word_bank'][randint(0, len( db['word_bank'] )  - 1 )],
-            'date': strftime("%a, %d %b %Y %H:%M", gmtime())
+            'date': strftime("%a, %d %b %Y %H:%M", gmtime()),
+            'feedback': []
         }
         
         
@@ -77,30 +88,35 @@ def GETdoc( key ):
     
     # check()
     
-    if request.method == 'POST':
-        
-        for i in db['activeKeys']['keys']:
-        
-            if i['file_key'].lower() == key.lower():
-                
-                return jsonify({ 'msg': 'https://my-projects-andregarvin.c9users.io/' + key.lower(), 'status': True })
-        
-        # check()
-        return jsonify({ 'msg': 'Key not found or is expired.', 'status': None })
+    if key == 'TEST':
+        return 'no key'
     
+    else:
+        
+        if request.method == 'POST':
+            
+            for i in db['activeKeys']['keys']:
+            
+                if i['file_key'].lower() == key.lower():
+                    
+                    return jsonify({ 'msg': 'https://my-projects-andregarvin.c9users.io/' + key.lower(), 'status': True })
+            
+            # check()
+            return jsonify({ 'msg': 'Key not found or is expired.', 'status': None })
+        
+        
+        elif request.method == 'GET':
     
-    elif request.method == 'GET':
-
-        for i in db['activeKeys']['keys']:
-        
-            if i['file_key'].lower() == key.lower():
-                
-                # check()
-                return render_template('download.html', resp={ 'file_name': i['file_name'], 'file_size': i['file_size'], 'date': i['date'], 'file_key': key.lower() })
-        
-        # check()
-        return jsonify({ 'msg': 'Key-file is not active anymore or does not exist.', 'status': True })
-        # render_template('error.html', resp='Key-file is not active anymore or does not exist.')
+            for i in db['activeKeys']['keys']:
+            
+                if i['file_key'].lower() == key.lower():
+                    
+                    # check()
+                    return render_template('download.html', resp={ 'file_name': i['file_name'], 'file_size': i['file_size'], 'date': i['date'], 'file_key': key.lower() })
+            
+            # check()
+            return jsonify({ 'msg': 'Key-file is not active anymore or does not exist.', 'status': False })
+            # render_template('error.html', resp='Key-file is not active anymore or does not exist.')
 
 @app.route('/download')
 def download():
@@ -115,7 +131,30 @@ def download():
             return send_file( BytesIO( i['content'] ), attachment_filename=i['file_name'], as_attachment=True )
     
     # check()
-    return jsonify({ 'msg': 'error', 'statuz': True })
+    return jsonify({ 'msg': 'error', 'status': None })
+
+
+@app.route('/feedback/<key>', methods=['POST'])
+def feedback( key ):
+    
+    msg = request.args.get('msg')
+    
+    if len( msg ) != 0:
+        
+        for k in db['activeKeys']['keys']:
+            
+            if k['file_key'] == key:
+                
+                k['feedback'].append(msg)
+                
+                print '\n', k['feedback'], '\n'
+                
+                return jsonify({ 'msg': msg, 'status': True })
+                
+        return jsonify({ 'msg': 'stop it!', 'status': None })
+    
+    else:
+        return jsonify({ 'msg': 'stop it!', 'status': None })
 
 
 app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)),debug=True)
