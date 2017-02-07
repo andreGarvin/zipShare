3e94efa38d3e541b50e55fba120ed5dd55cbf805
Dre_ <andre.garvin@envirostudies.org>
2017-02-07 01:37:48 +0000

    add the mongo_db connection into zipshare

--- webserver.py
+++ webserver.py
@@ -1,30 +1,25 @@
 # server
 from flask import Flask, request, render_template, jsonify, send_file, redirect, url_for
 from time import gmtime, strftime       # time stamp
+from pymongo import MongoClient         # database 'mongodb' module
 from random import randint              # random moduel
 from io import BytesIO                  # upload and download
 import os                               # operating system
 
 
+
 # start app
 app = Flask(__name__)
-
-
-# this is 'fake db'
-db = {
-    'activeKeys': {    # active kyes being used
-        'count': 0,
-        'keys': []
-    },
-    'feedback': [], 
-    'word_bank': [ 'pillow', 'house', 'cat', 'key', 'dogs', 'umbrella', 'brother', 'car', 'spider', 'kill', 'code', 'snap', 'sister', 'paper', 'gutair', 'icon', 'tree', 'leaf', 'master', 'bird', 'phone', 'bottle', 'snake', 'bot', 'teeth', 'bite', 'data', 'react', 'cover', 'coat', 'goat', 'disc', 'vinyl', 'book', 'wallet', 'computer', 'cards', 'bob', 'baby', 'mario', 'classroom' ]
-}
-
+client = MongoClient('localhost', 27017)
+db = client.zipshare
 
 # upload and homepage route
 @app.route('/', methods=['GET', 'POST'])
 def home():
-
+    
+    # wordbank fr the redirect urls
+    word_bank = [ 'pillow', 'house', 'cat', 'key', 'dogs', 'umbrella', 'brother', 'car', 'spider', 'kill', 'code', 'snap', 'sister', 'paper', 'gutair', 'icon', 'tree', 'leaf', 'master', 'bird', 'phone', 'bottle', 'snake', 'bot', 'teeth', 'bite', 'data', 'react', 'cover', 'coat', 'goat', 'disc', 'vinyl', 'book', 'wallet', 'computer', 'cards', 'bob', 'baby', 'mario', 'classroom' ]
+    
     # if it is a post method
     if request.method == 'POST':
         
@@ -33,27 +28,29 @@ def home():
         content = file.read()   # reads the file content
         
         if len( file.filename ) != 0:
+            
             # file object/dictionary
             POST_data = {
-                'end_time': request.form.get('time')[0:1],
+                'end_time': request.form.get('time'),
                 'file_name': file.filename,
                 'content': content,
                 'file_size': len( content ),
-                'file_key': db['word_bank'][randint(0, len( db['word_bank'] )  - 1 )],
+                'file_key': word_bank[randint(0, len( word_bank )  - 1 )],
                 'date': strftime("%a, %d %b %Y %H:%M", gmtime()),
             }
         
             # checkd to see if the file_key is not in use or been assigned        
-            for j in db['activeKeys']['keys']:
-            # if the file key that was assigned is in use then reasign the file_key
-                while j['file_key'] == POST_data['file_key']:
-                    
-                    POST_data['file_key'] = db['word_bank'][randint(0, len( db['word_bank'] )  - 1 )]
+            for j in db.docshare.find():
+                # if the file key that was assigned is in use then reasign the file_key
+                    while j['file_key'] == POST_data['file_key']:
+                        POST_data['file_key'] = db['word_bank'][randint(0, len( fake_db['word_bank'] )  - 1 )]
         
             # incremnt to number active keys 
             # and appened the new key into active key
-            db['activeKeys']['count'] += 1
-            db['activeKeys']['keys'].append( POST_data )
+            db.docshare.insert_one( POST_data )
+            
+            # db['activeKeys']['count'] += 1
+            # db['activeKeys']['keys'].append( POST_data )
             
             # then redirect the user to the download page
             return render_template('download.html', resp={ 'file_name': POST_data['file_name'], 'file_size': len( content ), 'date': POST_data['date'], 'file_key': POST_data['file_key'] })
@@ -62,7 +59,7 @@ def home():
     return render_template('index.html', resp={ 'activeKeys': db['activeKeys']['keys'], 'len': db['activeKeys']['count'] })
 
 
-# displaying the down;oad html page 
+# displaying the download html page 
 @app.route('/<key>', methods=['GET', 'POST'])
 def GETdoc( key ):
     
@@ -83,14 +80,16 @@ def GETdoc( key ):
     # if the user makes a get request
     elif request.method == 'GET':
     
-        # then redirects the user to the download page
+        # then redirects the user to the front download page
+        # goes through each key
         for i in db['activeKeys']['keys']:
-            # goes through each key
             
             if i['file_key'].lower() == key.lower():
+                
                 # if the key matches of the file they wish to download    
                 # then redirect the to the download page
                 return render_template('download.html', resp={ 'file_name': i['file_name'], 'file_size': i['file_size'], 'date': i['date'], 'file_key': key.lower() })
+        
         # else returns  back  a error messg in json format
         return jsonify({ 'msg': 'Key-file is not active anymore or does not exist.', 'status': False })
 
@@ -129,7 +128,9 @@ def feedback():
         
         # appends the feeback msg
         # then retuns back of the msg and the status in json format
-        return db['feedback']({ 'msg': msg, 'key': key,  'status': True })
+        db.feedback.insert_one({ 'msg': msg, 'key': key,  'status': True })
+        
+        return True
         
     # the use makes a post request
     elif request.method == 'GET':
